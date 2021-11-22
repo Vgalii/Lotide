b68f6f6aac55e8848c6056aaac114a39db508b91
Vgalii <veronicaggalii@gmail.com>
2021-11-22 18:28:07 -0500

    Used Template Literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,7 @@
 const assertEqual = function(actual, expected) {
   let errorMessage = "🛑🛑🛑 " + actual + "!==" + expected;
-  let messageCorr = "Assertion Passed: ✅✅✅ " + actual + "===" + expected;
   if (actual === expected) {
-    console.log(messageCorr);
+    console.log(`Assertion Passed: ✅✅✅ ${actual} === ${expected}`);
   } else {
     console.assert(actual === expected, errorMessage);
   }
