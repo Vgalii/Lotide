f2a6d0475793855c4ce72063a5e3691a83626aab
Vgalii <veronicaggalii@gmail.com>
2021-11-24 17:21:32 -0500

    Created count only function

---
+++ countOnly.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  let errorMessage = "🛑🛑🛑 " + actual + "!==" + expected;
+  if (actual === expected) {
+    console.log(`Assertion Passed: ✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.assert(actual === expected, errorMessage);
+  }
+};
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+   
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 1);
+assertEqual(result1["Agouhanna"], undefined);
