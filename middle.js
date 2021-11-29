202028da25058279f3aff2f9d8369a4b4155117f
Vgalii <veronicaggalii@gmail.com>
2021-11-29 15:14:36 -0500

    Refactored middle function and added a test file for it

--- middle.js
+++ middle.js
@@ -1,24 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-
-  } return true;
-};
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅Assertion passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-// assertArraysEqual(["3", "5", 8], ["1", 2, 3]);
+const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = function(array) {
   let result = [];
@@ -35,6 +15,5 @@ const middle = function(array) {
   return result;
 };
 
-// console.log(middle([1, 2, 3, 4, 5, 6]));
+module.exports = middle;
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [4]);
